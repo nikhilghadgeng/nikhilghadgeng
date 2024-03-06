d3fe180fbe3b3a23813fb1db95f97f811a630edf
nikhilghadgeng <nikhilghadgeng007@gmail.com>
2024-03-06 15:37:32 +0530

    update

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("in script file")
+
+var val1 = 20;
+var val2 = 30;
+var sum = val1 + val2
+var mat = val1 * val2
+var div = val1 / val2
+var sub = val1 - val2
+console.log(sum)
+console.log(mat)
+console.log(div)
+console.log(sub)
+function collegeStudentName(studentName){
+    console.log("Hello" + " " + studentName);
+}
+collegeStudentName("aditay");
